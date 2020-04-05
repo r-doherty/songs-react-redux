24f4f094e71731dcfb23bc7bf55f5aba62f8368d
Ryan Doherty <rjd.atx@gmail.com>
2020-04-05 14:11:28 -0500

    Calling Action Creators from Components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import { combineReducers } from 'redux';
 // Static array - so no args - we never expect to change it in this case
 const songsReducer = () => {
     return [
-        { title: 'Rushing Back ft. Vera Blue (Live from Red Rocks)', duration: '4:45', artist: 'Flume' },
-        { title: "Everlong vs. What You Won't Do for Love", duration: '4:23', artist: 'Elise Truow' },
+        { title: 'Rushing Back', duration: '4:45', artist: 'Flume (ft. Vera Blue)' },
+        { title: "Go With The Flow", duration: '3:11', artist: 'Queens Of The Stone Age' },
         { title: 'Let it Happen', duration: '7:54', artist: 'Tame Impala' },
         { title: 'Reptilia', duration: '3:39', artist: 'The Strokes' }
     ];
